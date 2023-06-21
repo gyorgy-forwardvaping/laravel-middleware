ea0e2beefddd875488164f75437781a8c252c87d
gyorgy-forwardvaping <gyorgy@forwardvaping.com>
2023-06-21 11:44:06 +0100

    session practice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -21,8 +19,26 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index(Request $request) {
+        //request session
+        // $request->session()->put(['george' => 'student']);
+        //global session
+        // session(['dave' => 'instructor']);
+
+        // echo session('george');
+        // remove one
+        // $request->session()->forget('george');
+
+
+        //remove all
+        // $request->session()->flush();
+        // dd($request->session()->all());
+
+        // $request->session()->flash('message', 'post created'); store for short period of time
+        // $request->session()->reflash('message', 'post created'); store longer
+        // $request->session()->keep('message'); store 
+        return $request->session()->get('message');
+
+        // return view('home');
     }
 }
